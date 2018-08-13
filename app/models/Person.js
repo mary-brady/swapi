1f8639518cf1ae70907294815c26c6d5f5070e6b
Mary Brady <emceebrady@gmail.com>
2018-08-13 14:20:46 -0600

    project setup

---
+++ app/models/Person.js
@@ -0,0 +1,17 @@
+export default class Person {
+    constructor(reqData) {
+        this.name = reqData.name
+        this.height = reqData.height
+        this.mass = reqData.mass
+        this.haircolor = reqData.hair_color
+        this.skinColor = reqData.skin_color
+        this.eyeColor = reqData.eye_color
+        this.creationDate = reqData.birth_year
+        this.gender = reqData.gender
+        this.homeworld = reqData.homeworld
+        this.films = reqData.films
+        this.species = reqData.species
+        this.starships = reqData.starships
+        this.url = reqData.url
+    }
+}
